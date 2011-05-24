90248742610b7e8eb7bf4d3a2616b45571e3656f
Scott O'Hara <scott@oharagroup.net>
2011-05-24 12:13:23 +1000

    Added setup.sh script

--- README.md
+++ README.md
@@ -2,11 +2,7 @@ Installing
 ==========
 - cd ~/
 - git clone --recursive git://github.com/scotto74/dotfiles.git
-- ln -s dotfiles/.vim/autoload/autoload ~/.vim/autoload
-- ln -s dotfiles/.vim/bundle ~/.vim/bundle
-- ln -s dotfiles/.vimrc ~/.vimrc
-- ln -s dotfiles/.gitconfig ~/.gitconfig
-- ln -s dotfiles/.bashrc ~/.bashrc
+- cd dotfiles && setup.sh
 
 Dependencies
 ============
---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Symlinking .vim/autoload (pathogen)"
+ln -s ./.vim/autoload/autoload ~/.vim/autoload
+echo "Symlinking ./vim/bundle (plugins)"
+ln -s ./.vim/bundle ~/.vim/bundle
+echo "Symlinking ./vimrc"
+ln -s ./.vimrc ~/.vimrc
+echo "Symlinking .gitconfig"
+ln -s ./.gitconfig ~./gitconfig
+echo "Symlinking .bashrc"
+ln -s ./.bashrc ~/.bashrc
+echo "Done"
+exit 0
