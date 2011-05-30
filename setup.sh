bf3768d0ae317de530957e9cee9bad1fb3bb3c3d
Scott O'Hara <scott@oharagroup.net>
2011-05-30 13:35:59 +1000

    Expand full path of current directory

--- setup.sh
+++ setup.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(readlink "$(dirname "$0")")
+
 if [ ! -d ~/.vim ]; then
 	echo "Creating .vim directory"
 	mkdir ~/.vim
 fi
 
 echo "Symlinking .vim/autoload (pathogen)"
-ln -s $(dirname "$0")/.vim/autoload/autoload ~/.vim/autoload
+ln -s $SCRIPT_DIR/.vim/autoload/autoload ~/.vim/autoload
 echo "Symlinking ./vim/bundle (plugins)"
-ln -s $(dirname "$0")/.vim/bundle ~/.vim/bundle
+ln -s $SCRIPT_DIR/.vim/bundle ~/.vim/bundle
 echo "Symlinking ./vimrc"
-ln -s $(dirname "$0")/.vimrc ~/.vimrc
+ln -s $SCRIPT_DIR/.vimrc ~/.vimrc
 echo "Symlinking .gitconfig"
-ln -s $(dirname "$0")/.gitconfig ~/.gitconfig
+ln -s $SCRIPT_DIR/.gitconfig ~/.gitconfig
 echo "Symlinking .bashrc"
-ln -s $(dirname "$0")/.bashrc ~/.bashrc
+ln -s $SCRIPT_DIR/.bashrc ~/.bashrc
 echo "Symlinking .zshrc"
-ln -s $(dirname "$0")/.zshrc ~/.zshrc
+ln -s $SCRIPT_DIR/.zshrc ~/.zshrc
 
 if [ -d ~/.kde/share/apps/konsole ]; then
 	echo "KDE detected, symlinking Konsole Solarized"
-	ln -s $(dirname "$0")/konsole-colors-solarized/Solarized\ Dark.colorscheme ~/.kde/share/apps/konsole/Solarized\ Dark.colorscheme
-	ln -s $(dirname "$0")/konsole-colors-solarized/Solarized\ Light.colorscheme ~/.kde/share/apps/konsole/Solarized\ Light.colorscheme
+	ln -s $SCRIPT_DIR/konsole-colors-solarized/Solarized\ Dark.colorscheme ~/.kde/share/apps/konsole/Solarized\ Dark.colorscheme
+	ln -s $SCRIPT_DIR/konsole-colors-solarized/Solarized\ Light.colorscheme ~/.kde/share/apps/konsole/Solarized\ Light.colorscheme
 fi
 
 if [ -d ~/.gconf/apps/gnome-terminal/profiles ]; then
 	echo "Gnome detected, configuring Gnome Terminal Solarized"
-	cd $(dirname "$0")/gnome-terminal-colors-solarized
+	cd $SCRIPT_DIR/gnome-terminal-colors-solarized
 	./install.sh
 fi
 
